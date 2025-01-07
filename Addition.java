1b31be3c7ad1ffc9edcacc711eaedb44deb4e5cb
Anchal2003 <Sahuanchal22@gmail.com>
2025-01-07 10:51:44 +0530

    Add Addition

--- Addition.java
+++ Addition.java
@@ -1,7 +1,7 @@
 public class Addition{
     
- 	void Add(int num1, int num2){
-		System.out.print(num1+num2);	
-	}
+    void Add(int num1, int num2){
+       System.out.print(num1+num2);	
+   }
 
 }
